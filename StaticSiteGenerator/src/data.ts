49ce46130185f24778f39105c2c77a41274713e7
Mifka Rene <rene.mifka@gmail.com>
2024-03-29 18:56:30 +0100

    Added create table statements

--- StaticSiteGenerator/src/data.ts
+++ StaticSiteGenerator/src/data.ts
@@ -30,6 +30,69 @@ export class DB {
 
     private static async ensureTablesCreated(connection: Database): Promise<void> {
 
+        await connection.run(`create table if not exists User (
+            name text not null,
+            password text not null,
+            constraint PK_User primary key (name)
+        ) strict`);
+
+        await connection.run(`create table if not exists Theme (
+            userName text not null,
+            name text not null,
+            isPublic integer not null,
+            constraint PK_Theme primary key (name),
+            constraint PK_Theme primary key (userName),
+            constraint FK_User foreign key (userName) reference User(name)
+        ) strict`);
+
+        await connection.run(`create table if not exists Style(
+            property text not null,
+            value text not null,
+            constraint PK_Style primary key (property),
+            constraint PK_Style primary key (value)
+        ) strict`);
+
+        await connection.run(`create table if not exists ElementStyles (
+            tag text not null,
+            userName text not null,
+            themeName text not null,
+            styleProperty text not null,
+            styleValue text not null,
+            constraint PK_ElementStyles primary key (tag),
+            constraint PK_ElementStyles primary key (userName ),
+            constraint PK_ElementStyles primary key (themeName ),
+            constraint PK_ElementStyles primary key (styleProperty),
+            constraint PK_ElementStyles primary key (styleValue),
+            constraint FK_Theme foreign key (userName) reference Theme(userName),
+            constraint FK_Theme foreign key (themeName) reference Theme(name),
+            constraint FK_Style foreign key (styleProperty) reference Style(property),
+            constraint FK_Style foreign key (styleValue) reference Style(value)
+        ) strict`);
+
+        await connection.run(`create table if not exists Project (
+            name text not null,
+            userName text not null,
+            themeName text not null,
+            themeOwner text not null,
+            constraint PK_Project primary key (name),
+            constraint PK_Project primary key (userName),
+            constraint FK_Theme foreign key (themeName) reference Theme(name),
+            constraint FK_Theme foreign key (themeOwner) reference Theme(userName)
+        ) strict`);
+
+        await connection.run(`create table if not exists File (
+            projectName text not null,
+            userName text not null,
+            index integer not null,
+            path text not null,
+            constraint PK_File primary key (projectName),
+            constraint PK_File primary key (userName),
+            constraint PK_File primary key (index),
+            constraint PK_File primary key (path),
+            contraint FK_Project foreign key (projectName) reference Project(name),
+            contraint FK_Project foreign key (userName) reference Project(userName)
+        ) strict`);
+
     }
 }
 
