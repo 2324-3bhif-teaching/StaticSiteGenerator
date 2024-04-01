e12c2f42ca0cf3c792dc0cba38ee2ea71aea2775
Mifka Rene <rene.mifka@gmail.com>
2024-04-01 19:37:19 +0200

    Delete added

--- StaticSiteGenerator/src/routers/themesRouter.ts
+++ StaticSiteGenerator/src/routers/themesRouter.ts
@@ -37,8 +37,29 @@ themeRouter.get('/:username', async (req, res) => {
     res.send(themes);
 });
 
-themeRouter.get('/:username/:name', async (_, res) => {
-    throw new Error("Not Implemented");
+themeRouter.get('/:username/:name', async (req, res) => {
+    const user = req.params.username;
+    const themeName = req.params.name;
+    let db;
+    let themes : Theme|undefined;
+    try {
+        db = await DB.createDBConnection();
+        const stmt = await db.prepare("select * from Theme where isPublic != 0 and userName = ?1 and name = ?2");
+
+        await stmt.bind({ 1: user ,2:themeName});
+
+        themes = await stmt.get<Theme>();
+
+        await stmt.finalize();
+    } catch (error) {
+        res.sendStatus(StatusCodes.BAD_REQUEST);
+    } finally {
+        if (db) {
+            await db.close();
+        }
+    }
+
+    res.send(themes);
 });
 
 themeRouter.post('/', async (req, res) => {
@@ -78,12 +99,41 @@ themeRouter.post('/', async (req, res) => {
 });
 
 
-themeRouter.put('/:username/:name', async (_, res) => {
+themeRouter.put('/:username/:name', async (req, res) => {
     throw new Error("Not Implemented");
 });
 
 
-themeRouter.delete('/:username/:name', async (_, res) => {
-    throw new Error("Not Implemented");
+themeRouter.delete('/:username/:name', async (req, res) => {
+    const theme: String  = req.params.name;
+    const username: String = req.params.username;
+
+    let db;
+    try {
+        db = await DB.createDBConnection();
+
+        await DB.beginTransaction(db);
+
+        const stmt = await db.prepare("DELETE FROM Theme WHERE userName = ?1 AND name = ?2");
+
+        await stmt.bind({ 1: username, 2: theme});
+
+        await stmt.run();
+
+        await stmt.finalize();
+
+        await DB.commitTransaction(db);
+    } catch (error) {
+        if (db) {
+            await DB.rollbackTransaction(db);
+        }
+        res.sendStatus(StatusCodes.BAD_REQUEST);
+    } finally {
+        if (db) {
+            await db.close();
+        }
+    }
+
+    res.sendStatus(StatusCodes.CREATED);
 });
 
