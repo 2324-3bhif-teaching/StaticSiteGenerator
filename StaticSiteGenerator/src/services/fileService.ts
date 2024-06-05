8c876ab7b2713a8b92216f3664157e00430e4a3a
Paul Schein <p.schein@students.htl-leonding.ac.at>
2024-06-05 14:36:02 +0200

    added file service

---
+++ StaticSiteGenerator/src/services/fileService.ts
@@ -0,0 +1,31 @@
+import {ServiceBase} from "../database/serviceBase";
+import {Unit} from "../database/unit";
+
+export class FileService extends ServiceBase {
+
+        public constructor(unit: Unit) {
+            super(unit);
+        }
+
+        public async selectFilesOfProject(userName: string, projectId: number): Promise<File[]> {
+            return [];
+        }
+
+        public async insertFile(userName: string, projectId: number, fileName: string): Promise<boolean> {
+            return false;
+        }
+
+        public async deleteFile(userName: string, projectId: number, fileId: number): Promise<boolean> {
+            return false;
+        }
+
+        public async updateFileIndex(userName: string, projectId: number, fileId: number, newIndex: number): Promise<boolean> {
+            return false;
+        }
+}
+
+export interface File {
+    id: number,
+    index: number,
+    name: string
+}
