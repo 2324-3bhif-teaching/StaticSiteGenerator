e945d508866dabb9e7ebde332a7e0d34b7ad457e
Paul Schein <p.schein@students.htl-leonding.ac.at>
2024-06-06 14:24:16 +0200

    inserting and updating works

--- StaticSiteGenerator/src/services/fileService.ts
+++ StaticSiteGenerator/src/services/fileService.ts
@@ -1,5 +1,6 @@
 import {ServiceBase} from "../database/serviceBase";
 import {Unit} from "../database/unit";
+import {Statement} from "sqlite";
 
 export class FileService extends ServiceBase {
 
@@ -7,12 +8,31 @@ export class FileService extends ServiceBase {
             super(unit);
         }
 
-        public async selectFilesOfProject(userName: string, projectId: number): Promise<File[]> {
-            return [];
+        public async selectFilesOfProject(projectId: number): Promise<File[]> {
+            const stmt: Statement = await this.unit.prepare(`
+                select id, file_index, name from File 
+                where project_id = ?1`,
+                {1: projectId});
+            return (await stmt.all<{id: number, file_index: number, name: string}[]>())
+                .map(file => {
+                    return {
+                        id: file.id,
+                        index: file.file_index,
+                        name: file.name
+                    };
+                });
         }
 
-        public async insertFile(userName: string, projectId: number, fileName: string): Promise<boolean> {
-            return false;
+        public async insertFile(projectId: number, fileName: string): Promise<boolean> {
+            const stmt: Statement = await this.unit.prepare(`
+                select max(file_index) as maxIndex 
+                from File 
+                where project_id = ?1`, {1: projectId});
+            const maxIndex: number = (await stmt.get<{maxIndex: number}>())?.maxIndex ?? -1;
+            const stmt2: Statement = await this.unit.prepare(`
+                insert into File (file_index, name, project_id) 
+                values (?1, ?2, ?3)`, {1: maxIndex + 1, 2: fileName, 3: projectId});
+            return await this.executeStmt(stmt2);
         }
 
         public async deleteFile(userName: string, projectId: number, fileId: number): Promise<boolean> {
@@ -21,6 +41,10 @@ export class FileService extends ServiceBase {
 
         public async updateFileIndex(userName: string, projectId: number, fileId: number, newIndex: number): Promise<boolean> {
             return false;
+        }
+
+        public async getFilePath(fileId: number): Promise<string> {
+            return "";
         }
 }
 
---
+++ StaticSiteGenerator/test/services/fileService.spec.ts
@@ -0,0 +1,51 @@
+import {setupTestData} from "../database/testData";
+import {Unit} from "../../src/database/unit";
+import {ProjectService} from "../../src/services/projectService";
+import {FileService, File} from "../../src/services/fileService";
+import {ThemeService} from "../../src/services/themeService";
+import {DefaultTheme} from "../../src/constants";
+
+const files: File[] = [
+    {id: 1, index: 0, name: "test1"},
+    {id: 2, index: 1, name: "test2"},
+    {id: 3, index: 2, name: "test3"},
+    {id: 4, index: 3, name: "test4"},
+    {id: 5, index: 4, name: "test5"}
+];
+
+const projectId: number = 1;
+
+describe("FileService", () => {
+    beforeEach(async () => {
+        await setupTestData();
+        const unit: Unit = await Unit.create(false);
+
+        const themeService: ThemeService = new ThemeService(unit);
+        await themeService.insertTheme(DefaultTheme);
+
+        const projectService: ProjectService = new ProjectService(unit);
+        await projectService.insertProject("test", "test");
+
+        await unit.complete(true);
+    });
+
+    describe("insertFile", () => {
+        test('should insert a file', async () => {
+            const unit: Unit = await Unit.create(false);
+            const fileService: FileService = new FileService(unit);
+            for (const file of files) {
+                expect(await fileService.insertFile(projectId, file.name)).toBeTruthy();
+            }
+            await unit.complete(true);
+            expect(await selectFiles(projectId)).toStrictEqual(files);
+        });
+    });
+});
+
+async function selectFiles(projectId: number): Promise<File[]> {
+    const unit: Unit = await Unit.create(true);
+    const fileService: FileService = new FileService(unit);
+    const files: File[] = await fileService.selectFilesOfProject(projectId);
+    await unit.complete();
+    return files;
+}
