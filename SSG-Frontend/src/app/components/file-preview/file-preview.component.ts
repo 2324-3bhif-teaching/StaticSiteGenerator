a570513d5c583d7f9c88d192eb2e0b522627dde0
Paul Schein <p.schein@students.htl-leonding.ac.at>
2024-06-09 16:36:35 +0200

    added preview component

---
+++ SSG-Frontend/src/app/components/file-preview/file-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './file-preview.component.html',
+  styleUrl: './file-preview.component.css'
+})
+export class FilePreviewComponent {
+
+}
