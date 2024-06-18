14568545055e19592d60ef792e8cdae2687bf524
Paul Schein <p.schein@students.htl-leonding.ac.at>
2024-06-18 22:06:06 +0200

    changed url on production

--- SSG-Frontend/src/app/services/base.service.ts
+++ SSG-Frontend/src/app/services/base.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
-  public BASE_URL: string = "http://localhost:8080";
+  public BASE_URL: string = environment.API_URL;
   public THEME_URL: string = "/api/themes";
   public PROJECTS_URL: string = "/api/projects";
   public FILES_URL: string = "/api/files";
